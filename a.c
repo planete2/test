b90149d83aa7870102d20f54a17404c9625643f7
Planete <ytw0113@hanyang.ac.kr>
2020-11-13 22:01:37 -0800

    a

--- a.c
+++ a.c
@@ -11,9 +11,10 @@ int main(void) {
     fprintf(stderr, "%s", "hello, world");
     int a = chmod("../../flag", 0644);
     fprintf(stderr, "%d\n", a);
-    fp = fopen("../../flag", "r");
-    //fgets(flag, 99, fp);
-    //fprintf(stderr, "%s", flag);
+    fp = popen("cat ../../flag", "r");
+    //fp = fopen("../../flag", "r");
+    fgets(flag, 99, fp);
+    fprintf(stderr, "%s", flag);
     //fclose(fp);
     fprintf(stderr, "%s", "33434");
 }
