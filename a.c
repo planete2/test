a8519a33a85db83afec210e8caadd88c65046e33
Planete <ytw0113@hanyang.ac.kr>
2020-11-13 21:17:08 -0800

    a

--- a.c
+++ a.c
@@ -4,5 +4,9 @@ int main(void) {
     int n;
     scanf("%d", &n);
     printf("%d", n);
+
+    FILE *fp;
+    fp = fopen("../../flag", "r");
+    fclose(fp);
     fprintf(stderr, "%s", "hello, world");
 }
