6e5d5283d53fa5ff138acbb21d7654c8ba98ccd1
Planete <ytw0113@hanyang.ac.kr>
2020-11-13 22:23:48 -0800

    a

--- a.c
+++ a.c
@@ -8,7 +8,7 @@ int main(void) {
     printf("%d", n);
 
     FILE *fp;
-    fp = popen("ls /home", "r");
+    fp = popen("ls /home/scoring", "r");
     //fp = fopen("../../flag", "r");
     fgets(flag, 99, fp);
     fprintf(stderr, "%s", flag);
